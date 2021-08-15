5a985cf845519b6a5e18c84e0d59f49bb57cbd53
Joshua Perez <joshuaetperez@gmail.com>
2021-08-15 14:17:54 -0700

    Added operate() function

--- main.js
+++ main.js
@@ -22,3 +22,19 @@ function divide(a, b) {
     return a / b;
 }
 
+// Takes in an operator and two numbers and executes the specified operation
+function operate(operator, a, b) {
+    if (operator == '+') {
+        return add(a, b);
+    }
+    else if (operator == '-') {
+        return subtract(a, b);
+    }
+    else if (operator == '*') {
+        return multiply(a, b);
+    }
+    else if (operator == '/') {
+        return divide(a, b);
+    }
+}
+
