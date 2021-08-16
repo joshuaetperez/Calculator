d6108e86471336c2ec8f8f9e4898002b4771a342
Joshua Perez <joshuaetperez@gmail.com>
2021-08-15 17:01:44 -0700

    Added functionality to the equal sign (=) button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Takes in two numbers and returns the sum
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 // Takes in two numbers and returns the difference
@@ -17,7 +17,7 @@ function multiply(a, b) {
 // If the divisor is 0, return error message
 function divide(a, b) {
     if (b == 0) {
-        return console.log("Cannot divide by 0");
+        return 'Cannot divide by 0';
     }
     return a / b;
 }
@@ -40,17 +40,54 @@ function operate(operator, a, b) {
 
 // Takes the number the user clicked on and adds it to the display
 function addToDisplay(e) {
-    numberDisplayed = this.id;
-    const calcDisplay = document.querySelector('.display');
-    calcDisplay.textContent = numberDisplayed;
+    if (resetDisplay) {
+        calcDisplay.textContent = '0';
+        resetDisplay = false;
+    }
+    if (calcDisplay.textContent == '0') {
+        calcDisplay.textContent = this.id;
+    }
+    else {
+        calcDisplay.textContent += this.id;
+    }
+    currentNumberDisplayed = calcDisplay.textContent;
+    if (operatorStored == null) {
+        firstNumberDisplayed = currentNumberDisplayed;
+    }
+}
+
+// Takes the operator the user clicked on and stores it for later use
+function storeOperator(e) {
+    operatorStored = this.id;
+    resetDisplay = true;
+}
+
+function evaluate(e) {
+    const result = operate(operatorStored, firstNumberDisplayed, currentNumberDisplayed);
+    calcDisplay.textContent = result;
+    firstNumberDisplayed = result;
+    currentNumberDisplayed = result;
 }
 
 // Global variables
-let numberDisplayed = 0;
+let firstNumberDisplayed = 0;
+let currentNumberDisplayed = 0;
+let operatorStored = null;
+let resetDisplay = false;
 
 // DOM variables
-const numberButtons = document.querySelectorAll('button[class=number]');
+const calcDisplay = document.querySelector('.display');
+const numberButtons = document.querySelectorAll('.number, .zero');
+const operatorButtons = document.querySelectorAll('.operator');
+const evaluatorButton = document.querySelector('.evaluator');
 
+// Event listeners
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener('click', addToDisplay);
 });
+
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', storeOperator);
+});
+
+evaluatorButton.addEventListener('click', evaluate);
